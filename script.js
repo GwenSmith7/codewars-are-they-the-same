931be48d38bdb88677f15958c4ea889caee5f7ff
gs86 <44513004+GwenSmith7@users.noreply.github.com>
2022-03-11 09:29:58 -0500

    Init Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function comp(array1, array2) {
+  if (array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b);
+  array2.sort((a, b) => a - b);
+  return array1.map((v) => v * v).every((v, i) => v == array2[i]);
+} // end function
